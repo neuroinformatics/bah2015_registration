9fe3cd1c6029ab41a83f6d7ad5b53ddcf14e29db
nebula <miyamoto@brain.imi.i.u-tokyo.ac.jp>
2015-09-05 16:33:18 +0900

    add binding texture function

--- vtk_test/draw_brain.py
+++ vtk_test/draw_brain.py
@@ -1,11 +1,13 @@
 import vtk
-from vtk.util.colors import tomato
 
+obj_filename = 'mousebrain_small.obj'
+png_filename = 'CD21594.1-Prlr.png'
+plane_scale = 25
+plane_ratio = 385. / 614.
+
+# read obj file
 object = vtk.vtkOBJReader()
-
-object.SetFileName('mousebrain_small.obj')
-#object.SetFileName('teapot.obj')
-
+object.SetFileName(obj_filename)
 objectMapper = vtk.vtkPolyDataMapper()
 objectMapper.SetInputConnection(object.GetOutputPort())
 
@@ -14,28 +16,45 @@ objectActor.SetMapper(objectMapper)
 objectActor.GetProperty().SetRepresentationToWireframe();
 objectActor.GetProperty().SetColor(1.0, 0.0, 0.0)
 
-
+# draw plane
 plane = vtk.vtkPlaneSource()
 plane.SetCenter(0,0,0)
 plane.SetNormal(1.0, 0.0, 0.0)
-planeMapper = vtk.vtkPolyDataMapper()
+#planeMapper = vtk.vtkPolyDataMapper()
 
+
+# build texture for plane
+reader = vtk.vtkPNGReader()
+reader.SetFileName(png_filename)
+texture = vtk.vtkTexture()
+texture.SetInputConnection(reader.GetOutputPort())
+
+map_to_plane = vtk.vtkTextureMapToPlane()
+map_to_plane.SetInputConnection(plane.GetOutputPort())
+#map_to_plane.PreventSeamOn() 
+planeMapper = vtk.vtkPolyDataMapper()
+planeMapper.SetInputConnection(map_to_plane.GetOutputPort()) 
+
+
+# transform plane
 transform = vtk.vtkTransform()
 transform.Translate(5, 10, 5)
-transform.Scale(1, 20, 10)
+transform.Scale(1, plane_scale, plane_scale * plane_ratio)
 #transform.RotateWXYZ(45,0,1,0)
-transformFilter=vtk.vtkTransformPolyDataFilter()
+transformFilter = vtk.vtkTransformPolyDataFilter()
 transformFilter.SetTransform(transform)
 transformFilter.SetInputConnection(plane.GetOutputPort())
 transformFilter.Update()
-
 planeMapper.SetInputConnection(transformFilter.GetOutputPort())
+
 
 planeActor = vtk.vtkActor()
 planeActor.SetMapper(planeMapper)
-planeActor.GetProperty().SetColor(0.5, 0.5, 0.5)
+planeActor.SetTexture(texture)
+#planeActor.GetProperty().SetColor(0.5, 0.5, 0.5)
 
 
+# prepare rendering
 ren = vtk.vtkRenderer()
 ren.AddActor(objectActor)
 ren.AddActor(planeActor)
@@ -43,12 +62,11 @@ ren.SetBackground(0.0, 0.0, 0.0)
 
 renWin = vtk.vtkRenderWindow()
 renWin.AddRenderer(ren)
-renWin.SetWindowName('load obj')
-renWin.SetSize(1000, 1000)
+renWin.SetWindowName('Mouse Brain Viewer')
+renWin.SetSize(1200, 1200)
 
 iren = vtk.vtkRenderWindowInteractor()
 iren.SetRenderWindow(renWin)
 iren.Initialize()
 iren.Start()
-
 
